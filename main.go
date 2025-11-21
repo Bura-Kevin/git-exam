85999a79ec12271367da6cb876d717bc49374868
Bura-Kevin <kevin.bura@ynov.com>
2025-11-21 11:07:42 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome - Index</h1>")
+}
+
 func main() {
+	http.HandleFunc("/", IndexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
